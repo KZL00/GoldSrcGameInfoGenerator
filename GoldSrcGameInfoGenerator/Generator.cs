ad784252a9d9bb16c1462208760006c2b2f3fad3
KZL <31285862+KZL00@users.noreply.github.com>
2022-06-21 21:30:38 +0200

    small generator fix

--- GoldSrcGameInfoGenerator/Generator.cs
+++ GoldSrcGameInfoGenerator/Generator.cs
@@ -21,22 +21,22 @@ namespace GoldSrcGameInfoGenerator
                 ByteWriter.AddText(fs, "// Download: [WEBSITE]" + Environment.NewLine + Environment.NewLine);
 
                 // Project
-                ByteWriter.AddText(fs, "game " + MOD_NAME + Environment.NewLine);
-                ByteWriter.AddText(fs, "type " + MOD_TYPE + Environment.NewLine);
-                ByteWriter.AddText(fs, "icon " + MOD_ICON + Environment.NewLine);
+                ByteWriter.AddText(fs, "game \"" + MOD_NAME + "\"" + Environment.NewLine);
+                ByteWriter.AddText(fs, "type \"" + MOD_TYPE + "\"" + Environment.NewLine);
+                ByteWriter.AddText(fs, "icon \"" + MOD_ICON + "\"" + Environment.NewLine);
 
                 // DLLs
-                ByteWriter.AddText(fs, "gamedll " + GAME_DLL + Environment.NewLine);
-                ByteWriter.AddText(fs, "gamedll_linux " + GAME_DLL_LINUX + Environment.NewLine);
-                ByteWriter.AddText(fs, "gamedll_osx " + GAME_DLL_OSX + Environment.NewLine);
+                ByteWriter.AddText(fs, "gamedll \"" + GAME_DLL + "\"" + Environment.NewLine);
+                ByteWriter.AddText(fs, "gamedll_linux \"" + GAME_DLL_LINUX + "\"" + Environment.NewLine);
+                ByteWriter.AddText(fs, "gamedll_osx \"" + GAME_DLL_OSX + "\"" + Environment.NewLine);
 
                 // Developer
-                ByteWriter.AddText(fs, "developer " + DEVELOPER_NAME + Environment.NewLine);
-                ByteWriter.AddText(fs, "developer_url " + DEVELOPER_WEBPAGE + Environment.NewLine);
+                ByteWriter.AddText(fs, "developer \"" + DEVELOPER_NAME + "\"" + Environment.NewLine);
+                ByteWriter.AddText(fs, "developer_url \"" + DEVELOPER_WEBPAGE + "\"" + Environment.NewLine);
 
                 // Maps
-                ByteWriter.AddText(fs, "startmap " + STARTING_MAP + Environment.NewLine);
-                ByteWriter.AddText(fs, "trainmap " + TRAINING_MAP + Environment.NewLine);
+                ByteWriter.AddText(fs, "startmap \"" + STARTING_MAP + "\"" + Environment.NewLine);
+                ByteWriter.AddText(fs, "trainmap \"" + TRAINING_MAP + "\"" + Environment.NewLine);
             }
 
             MessageBox.Show("The file has been successfully generated.", "GoldSrc Game Info Generator", MessageBoxButtons.OK, MessageBoxIcon.Information);
